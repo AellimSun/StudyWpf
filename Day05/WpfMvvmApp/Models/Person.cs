4cdcc8ac74b905ab7466f62ca13aa4d1a33c5d83
nya <elim9010@gmail.com>
2022-06-07 12:48:14 +0900

    Mvvm 기초!

---
+++ Day05/WpfMvvmApp/Models/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMvvmApp.Helpers;
+
+namespace WpfMvvmApp.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private string email;   //멤버변수
+        private string Email    //멤버속성
+        { 
+            get => email; // get{return email;}
+            set
+            {
+                if (Commons.IsValidEmail(email))
+                {
+                    throw new Exception("Invalid Email");
+                }
+                else email = value; 
+            } 
+        } 
+
+        public DateTime date { get; set; }
+        public DateTime Date
+        {
+            get { return date; }
+            set
+            {
+                var result = Commons.CalcAge(value);
+                if (result > 135 || result < 0)
+                {
+                    throw new Exception("Invalid Date");
+                }
+                else date = value;
+            } 
+        }
+        public bool IsBirthday
+        {
+            get
+            {
+                return DateTime.Now.Year == Date.Year &&
+                  DateTime.Now.Month == Date.Month &&
+                  DateTime.Now.Day == Date.Day;
+            }
+        }
+        public bool IsAdult
+        {
+            get
+            {
+                return Commons.CalcAge(date) > 18; // 성인
+            }
+        }
+
+        public Person(string firstName, string lastName, string email, DateTime date)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Date = date;
+        }
+    }
+}
---
+++ Day05/WpfMvvmApp/ViewModels/RelayCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfMvvmApp.ViewModels
+{
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> execute; // 실행처리를 위한 generic
+        private readonly Predicate<T> canExecute;   // 실행여부 판단 generic   , <T> : c++에서 Template할 때 함
+
+        // 실행여부에 따라 이벤트 추가&삭제 이벤트 핸들러
+        public event EventHandler CanExecuteChanged 
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute?.Invoke((T)parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute((T)parameter);
+        }
+
+        /// <summary>
+        /// execute만 파라미터 받는 생성자
+        /// </summary>
+        /// <param name="execute"></param>
+        public RelayCommand(Action<T> execute) : this(execute, null) {  }
+
+        /// <summary>
+        /// execute, canExecute 파라미터로 받는 생성자
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+    }
+}
