3ec021f6b402790658df7a334d17521cc47eee15
nya <elim9010@gmail.com>
2022-06-08 10:37:04 +0900

    220608 지우기전 커밋

---
+++ Day06/WpfCaliburnApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfCaliburnApp.ViewModels
+{
+    public class MainViewModel : Screen
+    {
+        string name;
+
+        //public string Name
+        //{
+        //    get => name;
+        //    set
+        //    {
+        //        name = value;
+        //        NotifyOfPropertyChange(() => Name);
+        //        NotifyOfPropertyChange(() => CanSayHello);
+        //    }
+        //}
+        //public bool CanSayHello
+        //{
+        //    get => !string.IsNullOrEmpty(Name); //null이면 false, null아니면 true return
+        //}
+        //public void SayHello()
+        //{
+        //    MessageBox.Show($"Hello~ {Name}");
+        //}
+    }
+}
